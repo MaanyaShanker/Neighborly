6bd95bc6130b004eda993e099861e8c3821b575b
Maanya <maanyashanker30@gmail.com>
2020-04-16 01:56:37 -0400

    added logo and icon. Also set up home activity so we can create fragments.

---
+++ app/src/main/java/technogirl/maanyavictoria/neighborly/HomeActivity.java
@@ -0,0 +1,48 @@
+package technogirl.maanyavictoria.neighborly;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_materials:
+                    mTextMessage.setText(R.string.title_materials);
+                    return true;
+                case R.id.navigation_time:
+                    mTextMessage.setText(R.string.title_time);
+                    return true;
+                case R.id.navigation_money:
+                    mTextMessage.setText(R.string.title_money);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
